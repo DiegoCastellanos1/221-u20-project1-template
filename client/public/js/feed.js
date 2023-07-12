cc6705f18585a9a21e38eadc0f62667491d51d67
DiegoCastellanos1 <137915320+DiegoCastellanos1@users.noreply.github.com>
2023-07-12 13:16:13 -0400

    project1

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -1,47 +1,41 @@
-document.getElementById('portal_button').addEventListener('click', (event) => {
-    window.location = 'http://my.marist.edu';
-   
-})
+document.getElementById('portal_button').addEventListener('click', event => {
+    goToLocation('https://my.marist.edu/');
+  })
 
-
-
-
-let feedItem1 = {
-    title: 'title1',
-    body: 'body1',
-    linkUrl: 'linkUrl1',
-    imageUrl: 'imageUrl1',
+let feedNumber1 = {
+  Title: 'Gold Cup Updates and Scores',
+  Body: 'Mexico beats Costa Rica with a 2-0 score to face Jamaica in the semifinals, and USA beats Canada in a penalty shootout to go to semi finals and face Panama',
+  linkUrl: 'https://olympics.com/en/news/concacaf-gold-cup-2023-all-results-scores-standings-complete-list',
+  imageUrl: '/images/download-min.png'
 }
 
-
-
-
-let feedItem2 = {
-    title: 'title2',
-    body: 'body2',
-    linkUrl: 'linkUrl2',
-    imageUrl: 'imageUrl2',
+let feedNumber2 = {
+  Title: 'Chivas de Guadalajara',
+  Body: 'Las Chivas win their second game of the season with a 3-1 win over Atletico de San Luis and now they are first in the league',
+  linkUrl: '',
+  imageUrl: '/images/chivas-min.png'
 }
 
-
-
-
-let feedItem3 = {
-    title: 'title3',
-    body: 'body3',
-    linkUrl: 'linkUrl3',
-    imageUrl: 'imageUrl3',
+let feedNumber3 = {
+  Title: 'Soccer Transfers',
+  Body: 'Messi has been transfered to Inter Miami, and Mbappe does not wanna leave PSG so PSG is telling him that if he does not leave they are gonna sell his teamates',
+  linkUrl: '',
+  imageUrl: '/images/messi.png'
 }
 
+var currentStories = [feedNumber1, feedNumber2, feedNumber3]
 
+window.addEventListener("load", () => {
+  for(var i=0; i < currentStories.length; i++){
+    displayFeed(currentStories[i]);
 
+  }
+});
 
-var currentStories = [feedItem1, feedItem2, feedItem3];
-console.log(currentStories);
-
-
-
-
-function display(currentStories) {
-    document.getElementById(newsfeed).innerHTML = currentStories;
+function displayFeed(currentStories){
+  let feed = document.getElementById('newsfeed');
+  feed.innerHTML += "<span><img class ='myImage' src ='" + currentStories.imageUrl +"'>" +"</span>";
+  feed.innerHTML += "<span><h2><a href+'" + currentStories.linkUrl+ "'> " + currentStories.Title + "</a></h2></span>"
+  feed.innerHTML += "<span>" +currentStories.Body + "</span>";
+  feed.innerHTML += "<hr />"
 }
